e034916d2f21620fdbe2e5ce81f9f3e01346e581
Sandeep Gupta <sandy.pec@gmail.com>
2013-04-18 08:26:46 +0530

    Add a simple command to encode/decode a base64 string

---
+++ src/main/java/com/sangupta/andruil/commands/encode/Base64.java
@@ -0,0 +1,37 @@
+package com.sangupta.andruil.commands.encode;
+
+import com.sangupta.andruil.commands.base.AbstractCommand;
+import com.sangupta.jerry.util.ConsoleUtils;
+
+public class Base64 extends AbstractCommand {
+
+	@Override
+	public String getName() {
+		return "base64";
+	}
+
+	@Override
+	public String getHelpLine() {
+		return "Encode/decode a base64 string";
+	}
+
+	@Override
+	public void execute(String[] args) {
+		String mode = ConsoleUtils.readLine("Mode (encode/decode, e/d): ", false);
+		String text = ConsoleUtils.readLine("Text: ", false);
+		
+		if("e".equalsIgnoreCase(mode) || "encode".equalsIgnoreCase(mode)) {
+			System.out.println(new String(org.apache.commons.codec.binary.Base64.encodeBase64(text.getBytes())));
+			return;
+		}
+		
+		if("d".equalsIgnoreCase(mode) || "decode".equalsIgnoreCase(mode)) {
+			System.out.println(new String(org.apache.commons.codec.binary.Base64.decodeBase64(text)));
+			return;
+		}
+		
+		System.out.println("Invalid mode selected.");
+	}
+
+
+}
