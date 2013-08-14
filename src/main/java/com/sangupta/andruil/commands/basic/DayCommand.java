ff8e40576b7ef8cff744d2760d2b383a4d8ac247
Sandeep Gupta <sandy.pec@gmail.com>
2013-08-14 10:40:54 +0530

    Added DAY command to find day on a given date

---
+++ src/main/java/com/sangupta/andruil/commands/basic/DayCommand.java
@@ -0,0 +1,57 @@
+package com.sangupta.andruil.commands.basic;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.sangupta.andruil.commands.base.AbstractCommand;
+import com.sangupta.jerry.util.AssertUtils;
+import com.sangupta.jerry.util.ConsoleUtils;
+
+/**
+ * Find out the weekday today, or on a given date.
+ * 
+ * @author sangupta
+ *
+ */
+public class DayCommand extends AbstractCommand {
+	
+	private final String[] WEEKDAYS = { "Saturday", "Sunday", "Monday", "Tueday", "Wednesday", "Thursday", "Friday"};
+
+	@Override
+	public String getName() {
+		return "day";
+	}
+
+	@Override
+	public String getHelpLine() {
+		return "Display the day today or a given date";
+	}
+
+	@Override
+	public void execute(String[] arguments) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(System.currentTimeMillis());
+		int day = calendar.get(Calendar.DAY_OF_WEEK);
+		System.out.println("Today is " + WEEKDAYS[day]);
+		
+		String date = ConsoleUtils.readLine("Enter date (dd/MM/yyyy): ", true);
+		if(AssertUtils.isEmpty(date)) {
+			return;
+		}
+		
+		Date d = null;
+		try {
+			d = new SimpleDateFormat("dd/MM/yyyy").parse(date);
+		} catch(ParseException e) {
+			System.out.println("Date entered not in valid format.");
+			return;
+		}
+		
+		calendar.setTimeInMillis(d.getTime());
+		day = calendar.get(Calendar.DAY_OF_WEEK);
+		System.out.println("Day on " + date + " was " + WEEKDAYS[day]);
+	}
+
+}
